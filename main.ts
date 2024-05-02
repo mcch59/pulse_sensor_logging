66939d6492ee6999ff9b654b0da40f84b843232c
mcch59 <109598391+mcch59@users.noreply.github.com>
2024-05-02 16:59:01 +0200

    Entfernung Dauerhaft-Schleife, weil Verzögerungen auftreten

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    time_start = input.runningTime()
+    while (input.runningTime() - time_start < 60000) {
+        led.plotBarGraph(
+        pins.analogReadPin(AnalogPin.P2),
+        1023
+        )
+        datalogger.log(datalogger.createCV("Pulse_Sensor_Data", pins.analogReadPin(AnalogPin.P2)))
+    }
+})
 input.onButtonPressed(Button.B, function () {
     datalogger.deleteLog()
     basic.showIcon(IconNames.Skull)
@@ -11,22 +21,10 @@ basic.showLeds(`
     # . . . #
     . # # # .
     `)
-basic.forever(function () {
-    basic.showLeds(`
-        . # # # .
-        . . . # .
-        . . # . .
-        . . . . .
-        . . # . .
-        `)
-    if (input.buttonIsPressed(Button.A)) {
-        time_start = input.runningTime()
-        while (input.runningTime() - time_start < 60000) {
-            led.plotBarGraph(
-            pins.analogReadPin(AnalogPin.P2),
-            1023
-            )
-            datalogger.log(datalogger.createCV("Pulse_Sensor_Data", pins.analogReadPin(AnalogPin.P2)))
-        }
-    }
-})
+basic.showLeds(`
+    . # # # .
+    . . . # .
+    . . # . .
+    . . . . .
+    . . # . .
+    `)
